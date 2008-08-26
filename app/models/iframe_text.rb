0d03e900388ada9a5642f4088ad34ce54cf4cbe8
James Turnbull <james@lovedthanlost.net>
2008-08-26 12:42:36 +1000

    Removed incorrect comment

--- app/models/iframe_text.rb
+++ app/models/iframe_text.rb
@@ -1,7 +1,6 @@
 class IframeText
   
   def self.get_iframe_text(project)
-    # Find all of the projects that have enabled the "ticket emailer" plugin
     iframe_text = project.custom_values.detect {|v| v.custom_field_id == Setting.plugin_redmine_tab['iframe_text'].to_i}        
     iframe_text = iframe_text.value if iframe_text    
     
